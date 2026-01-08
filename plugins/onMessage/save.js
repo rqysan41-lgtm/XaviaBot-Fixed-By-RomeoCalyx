11768ece7b9428e44fac9f23d10060a425246210
mztbas277-create <mztbas277@gmail.com>
2026-01-08 15:57:36 +0200

    Update save.js

--- plugins/onMessage/save.js
+++ plugins/onMessage/save.js
@@ -1,6 +1,7 @@
 export default function ({ message }) {
     const { body, messageID, senderID, attachments } = message;
 
+    // إضافة الرسالة الواردة إلى بيانات البوت
     global.data.messages.push({
         body,
         messageID,
