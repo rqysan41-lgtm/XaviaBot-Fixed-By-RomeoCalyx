7bb7dfb8dbb14ca31083e866db142aeeab7bc575
rqysan41-lgtm <rqysan41@gmail.com>
2026-01-11 17:42:48 +0200

    Add بنج.js command file

--- plugins/commands/general/بنج.js
+++ plugins/commands/general/بنج.js
@@ -2,6 +2,6 @@ export default function ({ message }) {
     let timeStart = Date.now();
     message.send('').catch(_ => {
         let timeEnd = Date.now();
-        message.reply(`Pong! ${timeEnd - timeStart}ms`);
+        message.reply(`بنج! ${timeEnd - timeStart}ms`);
     })
 }
